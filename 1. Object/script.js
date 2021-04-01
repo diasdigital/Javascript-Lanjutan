72a7929108cad562dedd444a6d61544a3b83474a
diasdigital <diasdigital03@gmail.com>
2021-04-01 14:48:35 +0700

    2. Function Declaration

--- 1. Object/script.js
+++ 1. Object/script.js
@@ -1,20 +1,42 @@
 // Cara untuk membuat Object pada javascript
 
 // 1. Object Literal
-let player1 = {
-  nama: "Dias",
-  energi: 10,
-  makan: function (porsi) {
-    this.energi = this.energi + porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  },
-};
+// let player1 = {
+//   nama: "Dias",
+//   energi: 10,
+//   makan: function (porsi) {
+//     this.energi = this.energi + porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   },
+// };
 
-let player2 = {
-  nama: "Mina",
-  energi: 20,
-  makan: function (porsi) {
-    this.energi = this.energi + porsi;
+// let player2 = {
+//   nama: "Mina",
+//   energi: 20,
+//   makan: function (porsi) {
+//     this.energi = this.energi + porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   },
+// };
+
+// 2. Function Declaration
+function Player(nama, energi) {
+  let player = {};
+  player.nama = nama;
+  player.energi = energi;
+
+  player.makan = function (porsi) {
+    this.energi += porsi;
     console.log(`Halo ${this.nama}, selamat makan!`);
-  },
-};
+  };
+
+  player.main = function (jam) {
+    this.energi -= jam;
+    console.log(`Halo ${this.nama}, selamat bermain!`);
+  };
+
+  return player;
+}
+
+let dias = Player("Dias", 10);
+let mina = Player("Mina", 20);
