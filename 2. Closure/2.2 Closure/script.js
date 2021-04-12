9cf6de30f6a072a73a474b60581ecaa0285a4f77
diasdigital <diasdigital03@gmail.com>
2021-04-12 14:43:26 +0700

    Menambah penjelasan Factory Function

--- 2. Closure/2.2 Closure/script.js
+++ 2. Closure/2.2 Closure/script.js
@@ -13,16 +13,38 @@
 
 // ==================================================
 
-function init() {
-    return function(nama) {
-        console.log(nama);
-    }
-}
+// function init() {
+//   return function (nama) {
+//     console.log(nama);
+//   };
+// }
 
-let panggilNama = init();
-panggilNama('Sana');
+// let panggilNama = init();
+// panggilNama("Sana");
 
-// ini disebut dengan function factory
+// ini disebut dengan factory function
 // jadi kita bikin function yg mengembalikan sebuah function lain
 // lalu kita simpan itu kedalam sebuah variabel
 // karena kita simpan di variabel, function tersebut bisa anonymous
+
+// ==================================================
+// Contoh Factory Function
+
+function ucapkanSalam(waktu) {
+  return function (nama) {
+    console.log(`Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan!`);
+  };
+}
+
+let selamatPagi = ucapkanSalam("Pagi");
+let selamatSiang = ucapkanSalam("Siang");
+let selamatMalam = ucapkanSalam("Malam");
+
+selamatPagi("Nayeon");
+selamatMalam("Jihyo");
+
+// kita memanggil function ucapkanSalam, mengirim argumen yaitu pagi, siang, atau malam
+// function tersebut akan mengembalikan sebuah function lagi yang kita simpan kedalam variabel
+// ketika kita memanggil function yang sudah dimasukan kedalam variabel tadi, kita mengirim argumen berupa nama
+// sehingga nama akan terisi oleh parameternya inner function
+// sedangkan waktu akan terisi oleh parameternya outer function dengan lexical scope
