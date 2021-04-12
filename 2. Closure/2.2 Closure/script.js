d30364a3d1ededa6d1eaf61b65a44ffcfdad047e
diasdigital <diasdigital03@gmail.com>
2021-04-12 10:49:31 +0700

    Menambah contoh function factory

--- 2. Closure/2.2 Closure/script.js
+++ 2. Closure/2.2 Closure/script.js
@@ -1,12 +1,28 @@
-function init() {
-    let nama = 'Mina'; // local variabel
-    function tampilNama() {
-        console.log(nama);
-    }
-    tampilNama();
-}
+// function init() {
+//     let nama = 'Mina'; // local variabel
+//     function tampilNama() {
+//         console.log(nama);
+//     }
+//     tampilNama();
+// }
 
-init();
+// init();
 
 // lexical scope adalah ketika sebuah function didalam function mencari sebuah variabel di parent nya
 // ini juga membuat function itu menjadi closure
+
+// ==================================================
+
+function init() {
+    return function(nama) {
+        console.log(nama);
+    }
+}
+
+let panggilNama = init();
+panggilNama('Sana');
+
+// ini disebut dengan function factory
+// jadi kita bikin function yg mengembalikan sebuah function lain
+// lalu kita simpan itu kedalam sebuah variabel
+// karena kita simpan di variabel, function tersebut bisa anonymous
