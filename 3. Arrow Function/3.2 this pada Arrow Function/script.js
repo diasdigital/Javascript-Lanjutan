bcc5d071e9bb553b74e6a3a05d35644806b1af61
diasdigital <diasdigital03@gmail.com>
2021-04-14 16:29:16 +0700

    Menjelaskan this Pada object literal yang memiliki arrow function

--- 3. Arrow Function/3.2 this pada Arrow Function/script.js
+++ 3. Arrow Function/3.2 this pada Arrow Function/script.js
@@ -13,11 +13,33 @@
 // ==============================
 // Arrow Function
 
-const Siswa = function () {
-  this.nama = 'Jeongyeon';
-  this.umur = 25;
-  this.sayHello = () => {
-    console.log(`Halo nama saya ${this.nama}, dan saya ${this.umur}`)
-  }
-}
-const jeongyeon = new Siswa;
+// const Siswa = function () {
+//   this.nama = 'Jeongyeon';
+//   this.umur = 25;
+//   this.sayHello = () => {
+//     console.log(`Halo nama saya ${this.nama}, dan saya ${this.umur} tahun`)
+//   }
+// }
+// const jeongyeon = new Siswa;
+
+// ------------------------------
+// Object Literal
+
+const siswa1 = {
+  nama: "Jihyo",
+  umur: 24,
+  sayHello: () => {
+    console.log(`Halo nama saya ${this.nama}, dan saya ${this.umur} tahun`);
+  },
+};
+
+// Catatan
+// Arrow Function tidak memiliki this
+// jadi kalau di functionnya pake this, dia bakal nyari ke lexical scopenya buat variabel yang namanya this juga
+// kan di object literal gak pake this
+// jadi dia nyari ke atas lagi, sampai ke window
+
+// beda kalau kita bikin constructor function
+// ketika salah satu method yang menggunakan arrow function tidak bisa menemukan this yang dipanggil
+// maka ia akan mencari ke atas
+// dan di atas, alias di constructor functionnya, ada variabel this
